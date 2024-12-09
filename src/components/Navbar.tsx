310b7227d6198ec04f8a62eee4a425f1d208e32d
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2024-12-09 18:28:19 +0200

    init project

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Navbar() {
+  return (
+    <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
+      <nav className="flex justify-between items-center">
+        <Link href="/">
+          <Image src="/logo.png" alt="logo" width={144} height={30} />
+        </Link>
+
+        <div className="flex items-center gap-5"></div>
+      </nav>
+    </header>
+  );
+}
